f0fde774b96c3c7d2a99f6efa1e61586dd541635
Daniel <maciej.wiqira@gmail.com>
2017-10-21 13:48:33 +0200

    Search icon functional

--- app/src/main/java/com/example/maciejwikira/prgnv2/MainViewActivity.java
+++ app/src/main/java/com/example/maciejwikira/prgnv2/MainViewActivity.java
@@ -2,9 +2,12 @@ package com.example.maciejwikira.prgnv2;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,14 +18,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainViewActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private ListView paragonsListView;
     private ArrayList<Paragon> paragonsArray;
+    private SQLiteDatabase db;
+    private ParagonDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +63,8 @@ public class MainViewActivity extends AppCompatActivity
         paragonsListView = (ListView)findViewById(R.id.paragonsListView);
 
         //get reference do the database
-        ParagonDbHelper mDbHelper = new ParagonDbHelper(this);
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        mDbHelper = new ParagonDbHelper(this);
+        db = mDbHelper.getReadableDatabase();
 
         //declare what to get from db
         String[] cols = new String[]{
@@ -90,6 +99,46 @@ public class MainViewActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_view, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final MySearchView searchView = (MySearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setSubmitButtonEnabled(true);
+
+        searchView.setOnQueryTextListener(new MySearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                search(searchView.getQuery().toString());
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewCollapsedEventListener(new MySearchView.OnSearchViewCollapsedEventListener() {
+            @Override
+            public void onSearchViewCollapsed() {
+                String[] cols = new String[]{
+                        "_id",
+                        "name",
+                        "category",
+                        "value",
+                        "date",
+                        "img",
+                        "text"
+                };
+
+                //get cursor
+                Cursor c = db.query(true, ParagonContract.Paragon.TABLE_NAME, cols,null, null, null, null, null, null);
+
+                populateList(paragonsListView, c);
+
+                c.close();
+            }
+        });
+
         return true;
     }
 
@@ -104,8 +153,6 @@ public class MainViewActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }else if(id == R.id.action_filter){
-            return true;
-        }else if(id == R.id.action_search){
             return true;
         }
 
@@ -139,6 +186,8 @@ public class MainViewActivity extends AppCompatActivity
 
     private void populateList(ListView lv, Cursor c){
 
+        paragonsArray.clear();
+
         //fill array with paragon objects created from database data
         while(c.moveToNext()){
 
@@ -157,6 +206,64 @@ public class MainViewActivity extends AppCompatActivity
         //finally get and set adapter
         ParagonListAdapter paragonListAdapter = new ParagonListAdapter(this.getApplicationContext(), R.layout.paragon_list_item, paragonsArray);
         lv.setAdapter(paragonListAdapter);
+
+    }
+
+    private void search(String query){
+
+        mDbHelper = new ParagonDbHelper(this);
+        db = mDbHelper.getReadableDatabase();
+
+        String[] cols = new String[]{
+                "_id",
+                "name",
+                "category",
+                "value",
+                "date",
+                "img",
+                "text"
+        };
+
+        String itemContent;
+        String itemNameContent;
+
+        Cursor c;
+        int text_index;
+        int title_index;
+        ArrayList<Paragon> searchResults = new ArrayList<Paragon>();
+        Matcher matchName, matchContent;
+        Pattern pattern = Pattern.compile(query.toLowerCase());
+
+        c = db.query(true, ParagonContract.Paragon.TABLE_NAME, cols,null, null, null, null, null, null);
+
+        try {
+            while (c.moveToNext()) {
+
+                title_index = c.getColumnIndex(ParagonContract.Paragon.NAME);
+                itemNameContent = c.getString(title_index).toLowerCase();
+                matchName = pattern.matcher(itemNameContent);
+                text_index = c.getColumnIndex(ParagonContract.Paragon.CONTENT);
+                itemContent = c.getString(text_index);
+                itemContent.toLowerCase();
+                matchContent = pattern.matcher(itemContent);
+                if(matchName.find() || matchContent.find()){
+                    searchResults.add(new Paragon(
+                            c.getInt(c.getColumnIndex("_id")),
+                            c.getString(c.getColumnIndex("name")),
+                            c.getString(c.getColumnIndex("category")),
+                            c.getString(c.getColumnIndex("value")),
+                            c.getString(c.getColumnIndex("date")),
+                            c.getString(c.getColumnIndex("img")),
+                            c.getString(c.getColumnIndex("text"))
+                    ));
+                }
+            }
+        } finally {
+            c.close();
+        }
+
+        ParagonListAdapter paragonListAdapter = new ParagonListAdapter(this.getApplicationContext(), R.layout.paragon_list_item, searchResults);
+        paragonsListView.setAdapter(paragonListAdapter);
 
     }
 }
