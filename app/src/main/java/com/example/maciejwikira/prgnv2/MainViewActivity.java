4524104194ebfa330b1b998be8bc2105d1d59427
Daniel <maciej.wiqira@gmail.com>
2017-10-21 14:34:56 +0200

    Working fab menu. Changed colors

--- app/src/main/java/com/example/maciejwikira/prgnv2/MainViewActivity.java
+++ app/src/main/java/com/example/maciejwikira/prgnv2/MainViewActivity.java
@@ -33,6 +33,7 @@ public class MainViewActivity extends AppCompatActivity
     private SQLiteDatabase db;
     private ParagonDbHelper mDbHelper;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,14 +41,7 @@ public class MainViewActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
