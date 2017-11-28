e76f2358c10743c19fee7e027d8556cf01cb2885
Daniel <maciej.wiqira@gmail.com>
2017-11-28 16:49:20 +0100

    Finding paragon value further improved

--- app/src/main/java/com/example/maciejwikira/prgnv2/TextRecognitionFunctions.java
+++ app/src/main/java/com/example/maciejwikira/prgnv2/TextRecognitionFunctions.java
@@ -32,9 +32,9 @@ public class TextRecognitionFunctions {
     private boolean dateFound;
     private String paragonValue;
     private String paragonDate;
-    private Matcher match;
+    private Matcher matchValue, matchDate;
     //Wzory do wyszukiwania wartości zakupu :
-    private Pattern wholeValue2 = Pattern.compile("(s\\w*ma|su\\w*|\\w*uma)(\\s*)(pln|\\w*ln|pl\\w*)(\\s*)\\d+(,|\\.)\\d+");
+    private Pattern wholeValue2 = Pattern.compile("(s\\w*a|su\\w*|\\w*uma|\\w*ma)(\\s*)(pln|\\w*ln|pl\\w*)(\\s*)\\d+(,|\\.)\\d+");
     private Pattern wholeValue3 = Pattern.compile("([0-9]+,[0-9]+ pln)(.*?)");
     private Pattern theValue = Pattern.compile("([0-9]+(,|\\.)[0-9]+)");
     //Wzór do wyszukiwania daty :
@@ -47,100 +47,6 @@ public class TextRecognitionFunctions {
         paragonValue = null;
         paragonDate = null;
         prgnText = "";
-    }
-
-    //Funkcja wyszukująca wartość paragonu
-    private String searchForTheValue(SparseArray<TextBlock> items){
-
-        String val = "";    //inicjalizacja zmiennej przechowującej znalezioną wartość
-        String foundVal;    //zmienna tymczasowa przechowująca wartość
-
-        match = wholeValue2.matcher(prgnText.toLowerCase());
-        if(match.find() && valueFound == false){
-            valueFound = true;
-            foundVal = match.group().substring(0);  //zapisz pierwsze wystąpienie wyszukiwanej wartości do zmiennej
-
-            //pozyskanie wartości zakupu jako liczby ze znalezionego wyrażenia
-            match = theValue.matcher(foundVal);
-            match.find();
-            val = match.group().substring(0);
-        }
-
-
-      /*  //Pętla przeszukująca każdą linię sczytanego z obrazu tekstu
-        for (int i = 0; i < items.size(); ++i) {
-
-            TextBlock item = items.valueAt(i);  //pobranie linii tekstu
-
-            //Sprawdzanie, czy linia pasuje do danego wzorca
-            match = wholeValue3.matcher(item.getValue().toLowerCase());
-            if(match.find() && valueFound == false){    //jeśli znaleziono wyrażenie pasujące do wzorca i nie znaleziono żadnego wcześniej
-
-                valueFound = true;  //ustaw zmienną sygnalizującą znalezienie poszukiwanej wartości
-                foundVal = match.group().substring(0);  //zapisz pierwsze wystąpienie wyszukiwanej wartości do zmiennej
-
-                //pozyskanie wartości zakupu jako liczby ze znalezionego wyrażenia
-                match = theValue.matcher(foundVal);
-                match.find();
-                val = match.group().substring(0);
-
-
-            }
-
-            //Sprawdzanie, czy linia pasuje do danego wzorca
-            match = wholeValue2.matcher(item.getValue().toLowerCase());
-            if(match.find() && valueFound == false){
-
-                valueFound = true;
-                foundVal = match.group().substring(0);
-
-                match = theValue.matcher(foundVal);
-                match.find();
-                val = match.group().substring(0);
-
-            }
-
-            if(valueFound == true){ // jeśli znaleziono wartość
-
-                //Wyświetlenie potwierdzenia odnalezienia wartości
-                Toast toast = Toast.makeText(context, "Znaleziono wartosc ! : " +  val , Toast.LENGTH_SHORT);
-                toast.show();
-
-                return val; // zwrócenie znalezionej wartości
-
-            }
-
-        }
-
-        */
-
-        return val;
-    }
-
-    //Funkcja wyszukująca datę
-    private String searchForTheDate(SparseArray<TextBlock> items){
-
-        String foundDate = "";  //inicjalizacja zmiennej przechowującej datę
-
-        for (int i = 0; i < items.size(); ++i) {    //sprawdzanie każdej linii w poszukiwaniu daty
-
-            TextBlock item = items.valueAt(i);  // pobranie linii tekstu
-
-            //Sprawdzenie czy w tekście znajduje się wyrażenie pasujące do wzorca
-            match = theDate.matcher(item.getValue().toLowerCase());
-            if (match.find() && dateFound == false) {   //jeśli znaleziono wystąpienie i nie znaleziono jeszcze daty
-
-                dateFound = true;   //ustaw zmienną sygnalizującą znalezienie daty
-                foundDate = match.group().substring(0); //zapisz znalezioną datę
-
-                //Wyświetl potwierdzenie znalezienia daty
-                Toast toast = Toast.makeText(context, "Znaleziono datę ! : " +  foundDate , Toast.LENGTH_SHORT);
-                toast.show();
-
-            }
-        }
-
-        return foundDate;    //zwróć znalezioną datę
     }
 
     // Metoda typu GET pozwala na pobranie ścieżki do przetwarzanego obrazu
@@ -177,29 +83,43 @@ public class TextRecognitionFunctions {
         // Inicjalizacja obszaru rozpoznawania
         Frame frame = new Frame.Builder().setBitmap(bitmap).build();
 
-
-
         // Zapisanie sczytanego tekstu
         SparseArray<TextBlock> items = textRecognizer.detect(frame);
         prgnText += "\n";
         for(int i = 0; i < items.size(); i++){
             TextBlock item = items.valueAt(i);
-            match = wholeValue2.matcher(item.getValue().toLowerCase());
-            if(match.find() && valueFound == false){
-                valueFound = true;
-                match = theValue.matcher(item.getValue().toLowerCase());
-                match.find();
-                paragonValue = match.group().substring(0);
-            }
+
+            //wyszukiwanie wartości w sczytanym tekście
+            //checkForValue(item.getValue().toLowerCase());
+
+            //wyszukiwanie daty w sczytanym tekście
+            //checkForDate(item.getValue().toLowerCase());
+
             prgnText += item.getValue().toLowerCase();
+
         }
 
-        if(valueFound == false) //jeśli nie znaleziono wartości
-            paragonValue = searchForTheValue(items);   // wyszukaj wartość w odczytanych danych
+        checkForValue(prgnText);
+        checkForDate(prgnText);
+    }
 
-        if(dateFound == false)  //jeśli nie znaleziono daty
-            paragonDate = searchForTheDate(items); //wyszukaj datę w odczytanych danych
+    private void checkForValue(String text){
+        matchValue = wholeValue2.matcher(text);
+        if(matchValue.find() && valueFound == false){
+            valueFound = true;
+            String temp = matchValue.group().substring(0);
+            matchValue = theValue.matcher(temp);
+            matchValue.find();
+            paragonValue = matchValue.group().substring(0);
+        }
+    }
 
+    private void checkForDate(String text) {
+        matchDate = theDate.matcher(text);
+        if (matchDate.find() && dateFound == false) {   //jeśli znaleziono wystąpienie i nie znaleziono jeszcze daty
+            dateFound = true;   //ustaw zmienną sygnalizującą znalezienie daty
+            paragonDate = matchDate.group().substring(0); //zapisz znalezioną datę
+        }
     }
 
     // Metoda typu SET pozwalająca na ustawienie ścieżki przetwarzanego obrazu
